7948066944ac1974d64fc32fa376fc34e107b717
shourya singh <138572082+shourya9058@users.noreply.github.com>
2024-11-14 15:22:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,115 @@
+const btn = document.querySelector('.talk')
+const content = document.querySelector('.content')
+
+
+function speak(text){
+    const text_speak = new SpeechSynthesisUtterance(text);
+
+    text_speak.rate = 1;
+    text_speak.volume = 1;
+    text_speak.pitch = 1;
+
+    window.speechSynthesis.speak(text_speak);
+}
+
+function wishMe(){
+    var day = new Date();
+    var hour = day.getHours();
+
+    if(hour>=0 && hour<12){
+        speak("Good Morning Boss...How can I help you today?")
+    }
+
+    else if(hour>12 && hour<17){
+        speak("Good Afternoon Boss...How can I help you today?")
+    }
+
+    else{
+        speak("Good Evenining Boss...How can I help you today?")
+    }
+
+}
+
+window.addEventListener('load', ()=>{
+    speak("Initializing Friday..");
+    wishMe();
+});
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition =  new SpeechRecognition();
+
+recognition.onresult = (event)=>{
+    const currentIndex = event.resultIndex;
+    const transcript = event.results[currentIndex][0].transcript;
+    content.textContent = transcript;
+    takeCommand(transcript.toLowerCase());
+
+}
+
+btn.addEventListener('click', ()=>{
+    content.textContent = "Listening...."
+    recognition.start();
+})
+
+function takeCommand(message){
+    if(message.includes('hey') || message.includes('hello')){
+        speak("Hello Boss, How May I Help You?");
+    }
+    else if(message.includes("open google")){
+        window.open("https://google.com", "_blank");
+        speak("Opening Google...")
+    }
+
+    else if (message.includes("tell me about yourself")) {
+        speak("Hello! I’m Friday, your personal AI assistant, here to keep you organized and entertained. I was created by Shaurya Singh, Dhruv Khanna, and Nishant Gupta on November 14th, 2024 which is Children’s Day, which makes me the most hardworking child around! I handle the details, find answers, and can even crack a joke or two. Think of me as part assistant, part guide, and a dash of comedian. So, what can I help you with today?");
+    }
+    
+
+
+    else if(message.includes("open youtube")){
+        window.open("https://youtube.com", "_blank");
+        speak("Opening Youtube...")
+    }
+    else if(message.includes("open facebook")){
+        window.open("https://facebook.com", "_blank");
+        speak("Opening Facebook...")
+    }
+
+    else if(message.includes('what is') || message.includes('who is') || message.includes('what are')) {
+        window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
+        const finalText = "This is what i found on internet regarding " + message;
+	    speak(finalText);
+  
+    }
+
+    else if(message.includes('wikipedia')) {
+        window.open(`https://en.wikipedia.org/wiki/${message.replace("wikipedia", "")}`, "_blank");
+        const finalText = "This is what i found on wikipedia regarding " + message;
+        speak(finalText);
+    }
+
+    else if(message.includes('time')) {
+        const time = new Date().toLocaleString(undefined, {hour: "numeric", minute: "numeric"})
+        const finalText = time;
+        speak(finalText);
+    }
+
+    else if(message.includes('date')) {
+        const date = new Date().toLocaleString(undefined, {month: "short", day: "numeric"})
+        const finalText = date;
+        speak(finalText);
+    }
+
+    else if(message.includes('calculator')) {
+        window.open('Calculator:///')
+        const finalText = "Opening Calculator";
+        speak(finalText);
+    }
+
+    else {
+        window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
+        const finalText = "I found some information for " + message + " on google";
+        speak(finalText);
+    }
+}
